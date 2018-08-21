b41e364dcb0a3449263eb85ea4dfeb6399ac80ff
ZouZLong <1715011480@qq.com>
2018-08-21 16:37:35 +0800

    完成

--- app/src/main/java/zzl/imageoptimization/MainActivity.java
+++ app/src/main/java/zzl/imageoptimization/MainActivity.java
@@ -1,13 +1,42 @@
 package zzl.imageoptimization;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.widget.GridView;
 
-public class MainActivity extends AppCompatActivity {
+import zzl.imageoptimization.adapter.PhotoWallAdapter;
+import zzl.imageoptimization.data.Images;
+
+public class MainActivity extends Activity {
+
+    /**
+     * 用于展示照片墙的GridView
+     */
+    private GridView mPhotoWall;
+
+    /**
+     * GridView的适配器
+     */
+    private PhotoWallAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPhotoWall = findViewById(R.id.photo_wall);
+
+        adapter = new PhotoWallAdapter(this, 0, Images.imageThumbUrls, mPhotoWall);
+
+        mPhotoWall.setAdapter(adapter);
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        // 退出程序时结束所有的下载任务
+        adapter.cancelAllTasks();
+    }
+
 }
+
---
+++ app/src/main/java/zzl/imageoptimization/data/Images.java
@@ -0,0 +1,53 @@
+package zzl.imageoptimization.data;
+
+
+/**
+ * 图片的来源
+ */
+
+public class Images {
+    public final static String[] imageThumbUrls = new String[]{
+            "http://img17.3lian.com/d/file/201701/16/828d099209191784e20568ae4163384f.jpg",
+            "http://www.pig66.com/uploadfile/2017/1116/20171116103235483.png",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074052947.jpg",
+             "http://www.pig66.com/uploadfile/2017/1116/20171116103248281.png",
+            "http://img17.3lian.com/d/file/201701/16/2903447217bdc2a804e2d37487627032.jpg",
+            "http://new-img2.ol-img.com/985x695/118/675/li7knjlp2kI.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074131486.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074054550.jpg",
+            "http://img16.3lian.com/gif2016/q10/16/1.jpg",
+            "http://pic1.win4000.com/wallpaper/c/57c10cd85ea5f.jpg",
+            "http://img16.3lian.com/gif2016/q21/81/109.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511075705684.jpg",
+            "http://imgsrc.baidu.com/imgad/pic/item/cdbf6c81800a19d89a34ca3c38fa828ba61e46d7.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074204129.jpg",
+            "http://img16.3lian.com/gif2016/q3/25/101.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074054706.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074101960.jpg",
+            "http://img17.3lian.com/d/file/201702/08/7f5ffdca69a79baab307aae1d09fc696.jpg",
+            "http://t-1.tuzhan.com/0b917ad68901/c-2/l/2013/10/15/18/ef930250a9d042dbba937129e990a5c8.jpg",
+            "http://uploadfile.bizhizu.cn/2014/0710/20140710032707578.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511115551762.jpg",
+            "http://new-img2.ol-img.com/375x375/120/88/liqRqIsDvF0yk.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074053902.jpg",
+            "http://img17.3lian.com/d/file/201701/16/355fc6379da5c087cfc342b317bf3d76.jpg",
+            "http://img17.3lian.com/d/file/201702/08/126bfda91b30b07aff91f97bbba3fb3d.jpg",
+            "http://uploads.5068.com/allimg/1807/186-1PI01F240.jpg",
+            "http://www.pig66.com/uploadfile/2017/1116/20171116103244456.png",
+            "http://s10.sinaimg.cn/mw690/002h4R6ngy6Ty53XRB789&690",
+            "http://www.zx1234.com/uploads/allimg/180418/16-1P41Q20143-50.jpg",
+            "http://demo.kesion.com/UploadFiles/2014-08/2/2014841638210.jpg",
+            "http://t-1.tuzhan.com/8334f9499dd7/c-2/l/2013/08/08/13/45897081005345409370ffcc200ac521.jpg",
+            "http://img.duoziwang.com/2016/12/08/18285125544.jpg",
+            "http://baiducdn.pig66.com/uploadfile/2017/0511/20170511074054550.jpg",
+            "http://img4.imgtn.bdimg.com/it/u=2200863405,3653337599&fm=27&gp=0.jpg",
+            "http://pic1.win4000.com/wallpaper/2017-10-12/59dee000a84f0.jpg",
+            "http://pic1.win4000.com/wallpaper/4/57c0f6ba678af.jpg",
+            "http://y0.ifengimg.com/40179b07a3d3a3d9/2012/0528/rdn_4fc2ea62349b4.jpg",
+            "http://pic1.win4000.com/wallpaper/9/5879e4ad702c3.jpg",
+            "http://img.t.388g.com/3/images/201610/1476927651134043.jpg"
+    };
+}
+
+
+
